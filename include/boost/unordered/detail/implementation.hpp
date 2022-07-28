6f7b5f864cd5ac7b30d742440ff785978c7b3c46
Christian Mazakas <christian.mazakas@gmail.com>
2022-07-27 18:59:25 -0700

    Fix bad attempt at git stashing/incremental staging

--- include/boost/unordered/detail/implementation.hpp
+++ include/boost/unordered/detail/implementation.hpp
@@ -2534,19 +2534,6 @@ namespace boost {
           node_pointer p = itb->next;
           BOOST_ASSERT(!p || (p && p->first_in_group()));
 
-          for (; p; p = p->next()) {
-            if (boost::is_same<node_pointer, node_type*>::value) {
-              if (!p->first_in_group()) {
-                continue;
-              }
-            }
-        template <class Key>
-        node_pointer find_node_impl(Key const& x, bucket_iterator itb) const
-        {
-          key_equal const& pred = this->key_eq();
-          node_pointer p = itb->next;
-          BOOST_ASSERT(!p || (p && p->first_in_group()));
-
           for (; p;) {
             if (pred(x, this->get_key(p))) {
               BOOST_ASSERT(p->first_in_group());
